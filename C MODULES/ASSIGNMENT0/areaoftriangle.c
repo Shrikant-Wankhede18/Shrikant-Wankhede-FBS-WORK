77866e24219f48d7abf58435693c01c88349efc0
Shrikant Wankhede <shrikantwankhede1817@gmail.com>
2025-07-28 20:56:16 +0530

    Add files via upload

---
+++ C MODULES/ASSIGNMENT0/areaoftriangle.c
@@ -0,0 +1,7 @@
+//Write a C program to input the base and height of a triangle and calculate its area.
+#include<stdio.h>
+void main(){
+	int base=10,height=20;
+	double area=0.5*base*height;
+	printf("area of triangle is %f",area);
+}
