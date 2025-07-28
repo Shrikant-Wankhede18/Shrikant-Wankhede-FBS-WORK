16a8a3a055e5aa782a3824b90b6b8b36842545db
Shrikant Wankhede <shrikantwankhede1817@gmail.com>
2025-07-28 20:53:11 +0530

    Add files via upload

---
+++ C MODULES/ASSIGNMENT0/05findavg.c
@@ -0,0 +1,7 @@
+//Write a C program to input five numbers and find their average.
+#include<stdio.h>
+void main(){
+	float eng=74.35,math=35.49,science=80.73,marathi=60.00,hindi=80.90;
+	float average=(eng+math+science+marathi+hindi)/5;
+	printf("the average is : %f",average);
+}
