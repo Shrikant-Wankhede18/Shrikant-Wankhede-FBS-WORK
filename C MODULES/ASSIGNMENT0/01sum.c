166810abf0d330bdd7e7a436e2f4ed84a03d39e2
Shrikant Wankhede <shrikantwankhede1817@gmail.com>
2025-07-28 20:50:19 +0530

    Add files via upload

---
+++ C MODULES/ASSIGNMENT0/01sum.c
@@ -0,0 +1,8 @@
+//Write a C program to add two integers and display the result
+#include<stdio.h>
+void main(){
+	int a=10;
+	int b=20;
+	int c=a+b;
+	printf("addition of: %d and : %d is %d",a,b,c);
+}
