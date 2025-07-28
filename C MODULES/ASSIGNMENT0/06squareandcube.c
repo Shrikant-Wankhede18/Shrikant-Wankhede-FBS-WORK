79eb84df0a258e378c9c2462a8a37853232a2ac0
Shrikant Wankhede <shrikantwankhede1817@gmail.com>
2025-07-28 20:54:29 +0530

    Add files via upload

---
+++ C MODULES/ASSIGNMENT0/06squareandcube.c
@@ -0,0 +1,10 @@
+//Write a C program to find the square and cube of a given number.
+#include<stdio.h>
+void main(){
+	int a=12;
+	int square;
+	int cube;
+	square=a*a;
+	cube=a*a*a;
+	printf("the square is : %d and the cube is : %d",square,cube);
+}
