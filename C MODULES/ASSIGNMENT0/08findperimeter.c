22d5d6fe7ce01d52a3ae8f7afae5cfdfea87d473
Shrikant Wankhede <shrikantwankhede1817@gmail.com>
2025-07-28 20:55:40 +0530

    Add files via upload

---
+++ C MODULES/ASSIGNMENT0/08findperimeter.c
@@ -0,0 +1,8 @@
+//Write a C program to input the length and width of a rectangle and find its perimeter.
+#include<stdio.h>
+void main(){
+	int len=20,width=10;
+	int p;
+	p=2*(len+width);
+	printf("the perimeter of rectangle is %d",p);
+}
