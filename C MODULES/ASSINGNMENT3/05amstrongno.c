fa599f9c39774e669cc3aa464de149e787b32db7
Shrikant Wankhede <shrikantwankhede1817@gmail.com>
2025-08-11 00:37:15 +0530

    Add files via upload

---
+++ C MODULES/ASSINGNMENT3/05amstrongno.c
@@ -0,0 +1,22 @@
+//Check the given number is Armstrong number or not..
+//Input: n = 153
+//Output: Armstrong
+#include<stdio.h>
+void main(){
+	int no=153,rem,sum=0;
+	int temp=no;
+	while(no>0)
+	{
+		rem=no%10;
+		sum=sum+(rem*rem*rem);
+		no=no/10;
+	} 
+	if(temp==sum)
+	{
+		printf("amrmstrong");
+	}
+	else
+	{
+		printf("not amrmstrong");
+	}
+}
