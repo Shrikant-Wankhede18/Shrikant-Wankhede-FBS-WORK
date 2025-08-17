4108c7bc00dff01a5717548811adf16d96ce1448
Shrikant Wankhede <shrikantwankhede1817@gmail.com>
2025-08-18 01:40:56 +0530

    Update 03searchnuminarray.c

--- C MODULES/ASSIGNMENT8/03searchnuminarray.c
+++ C MODULES/ASSIGNMENT8/03searchnuminarray.c
@@ -1,24 +1,24 @@
-//Search the given number in array.
+//2. Search the given number in array.
 #include<stdio.h>
-void main(){
-	int arr[5];
-	int brr[5];	
-	for(int i=0;i<5;i++){
-		printf("enter the number at %d index",i);
-		scanf("%d",&arr[i]);
-	}
-	printf("entered array arr[5]={");
-	for(int i=0;i<5;i++){
-		printf("%d",arr[i]);
-	}
-	printf("}\n");
-	
-	}
-	printf("entered array brr[5]={");
-	for(int k=0;k<5;k++){
-		printf("%d",brr[k]);
-	}
-	printf("}\n");
-	
-	
+void main() {
+	int arr[5]= {74,7,40,7,10};
+	int  n=7;
+	int flag=0;
+	for( int i=0; i<5; i++) {
+		printf("%d ",arr[i]);
+		}
+		printf("\n");
+    
+   for(int i=0;i<5;i++){
+   	  if(arr[i]==n){
+   	  printf("%d at %d index\n",arr[i],i);
+   	  flag=1;
+   	  
+		 }
+   	
+   }
+   if(flag==0)
+   printf(" No not Found");
+
+
 }
